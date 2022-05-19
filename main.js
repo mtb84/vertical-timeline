d2ba204f5893e8095fd328ab467e4f9061506e1e
MATTHEW TYLER BARTON <mtb@MATTHEWs-MacBook-Pro.local>
2022-05-19 02:23:17 -0400

    about halfway

---
+++ main.js
@@ -0,0 +1,14 @@
+const btnOpen1 = document.querySelector('.btn1')
+const btnOpen2 = document.querySelector('.btn2')
+// console.log(btnOpenModal1);
+// console.log(btnOpenModal2);
+
+// for(let i = 0; i< btnOpenModal1.length; i++)
+const btnOM1 = btnOpen1.addEventListener('click', function () {
+ console.log('clicked');
+});
+
+const btnOM2 = btnOpen2.addEventListener('click', function () {
+    console.log('clicked2');
+});
+
